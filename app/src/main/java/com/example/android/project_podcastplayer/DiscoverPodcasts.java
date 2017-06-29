396809a153f549e4199344a97a45594c504090d5
jamack <jamack9@hotmail.com>
2017-06-28 20:56:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/project_podcastplayer/DiscoverPodcasts.java
@@ -0,0 +1,13 @@
+package com.example.android.project_podcastplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiscoverPodcasts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover_podcasts);
+    }
+}
